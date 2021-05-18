e2575492b678ee22ec37cb010dbc999c54248dbc
Savanth P.S <84121119+SavanthPS@users.noreply.github.com>
2021-05-18 20:38:54 +0530

    Update test.py
    Was able to retrieve data from twitter

--- test.py
+++ test.py
@@ -1,18 +1,30 @@
-#helloo world
 
+#python libraries
+import datetime
+import csv
 import tweepy
-import urllib.request, urllib.parse, urllib.error
-import twurl
-import json
 
-key = '1393843058207203336-ajDwuNY9xVughPJdMoOJtLiKOopixx'
-secret = "gAFLk9vSsNzHip2V6t1cFekO65MgQ10jut1gsRVg6iXBZ"
+key = '139.............................moylzIju65f'
+secret = "LB0nB.................................GqhMyo"
 
-consumer_key = "Km9zRV1UtaDDvdiY8ZMSaQ2Jr"
-consumer_secret = "Gfeckp0tn3X2iFWc9USvkJjaH4EU9MaoVVm8ICXIOG3T9HrXkx"
+consumer_key = "rQM6ra......................ce17"
+consumer_secret = "tLEmpaA...........................wBfnq"
 
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
-
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
 api = tweepy.API(auth)
-for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
-    print(tweet.text)
+
+print("Hey there /n Enter the following Details : ")
+print("User name: ")
+name = input()
+print("from date ..................enter in yy-mm-dd  format : ")
+from_d = input()
+print("to until date ..................enter in yy-mm-dd  format : ")
+until_d = input()
+for tweet in tweepy.Cursor(api.search,
+                           q=name,
+                           since=from_d,
+                           until=until_d,
+                           lang="en").items():
+    print(tweet.created_at, tweet.text)
