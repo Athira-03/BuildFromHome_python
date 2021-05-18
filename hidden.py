8793f3da801d10e9872337ef306a333a83db1429
Savanth P.S <84121119+SavanthPS@users.noreply.github.com>
2021-05-18 12:45:59 +0530

    Delete hidden.py

--- hidden.py
+++
@@ -1,7 +0,0 @@
-
-
-def oauth():
-    return {"consumer_key": "r14f6qp0zWbpTt8kKwcM8ba5P",
-            "consumer_secret": "hWrGPn00mAptoJfcATAbayfwt9wwMX8Alzfd0YnAyF1pwH76Uw",
-            "token_key": "1393843058207203336-AE8SMIdcJjcobFCOGClbaYK6zrLGqR",
-            "token_secret": "gRDC1MZ6RHoa8ZikypOOKnV9roJdsO842SNjaS9IakL2g"}
