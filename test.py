71d56023b38bdf97660115609fcb972ade45a334
Annet-Maria <62251308+Annet-Maria@users.noreply.github.com>
2021-05-19 11:51:12 +0530

    Update test.py
    Edited the program. Changed the year format. 
    Now the csv file will be available in the file folder of the program.

--- test.py
+++ test.py
@@ -1,30 +1,40 @@
-
 #python libraries
 import datetime
 import csv
 import tweepy
 
-key = '139.............................moylzIju65f'
-secret = "LB0nB.................................GqhMyo"
+key = '13938430......................................84........................moylzIju65f'
+secret = "LB0nB.....................................................Myo"
 
-consumer_key = "rQM6ra......................ce17"
-consumer_secret = "tLEmpaA...........................wBfnq"
+consumer_key = "rQM6ra............................................hmce17"
+consumer_secret = "tLEmpa............................................cwBfnq"
 
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
 api = tweepy.API(auth)
 
+# Open/create a file to append data to
+csvFile = open('My Twitter Tweets.csv', 'a')
+
+#Use csv writer
+csvWriter = csv.writer(csvFile)
+
 print("Hey there /n Enter the following Details : ")
 print("User name: ")
 name = input()
-print("from date ..................enter in yy-mm-dd  format : ")
+print("from date ..................enter in yyyy-mm-dd  format : ")
 from_d = input()
-print("to until date ..................enter in yy-mm-dd  format : ")
+print("to until date ..................enter in yyyy-mm-dd  format : ")
 until_d = input()
 for tweet in tweepy.Cursor(api.search,
                            q=name,
                            since=from_d,
                            until=until_d,
                            lang="en").items():
-    print(tweet.created_at, tweet.text)
+
+    # Write a row to the CSV file. I use encode UTF-8
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+    print
+    tweet.created_at, tweet.text
+csvFile.close()
